991def7b644e8070df654e9b13e78ebdc62c9e4c
VladSebov <sebov.vlad@inbox.ru>
2025-07-01 07:39:50 +0300

    feat: enable showing messages only on looking at the door

--- Assets/Resources/ObjectCreation/Scripts/DoorLockController.cs
+++ Assets/Resources/ObjectCreation/Scripts/DoorLockController.cs
@@ -5,22 +5,22 @@ using UnityEngine.UI;
 public class DoorLockController : MonoBehaviour
 {
     private HingeJoint DoorHingeJoint;
-
     public Rigidbody DoorRigidbody;
-
     private static string CurrentDoor;
-    
     private int status = 0;
-
     public static bool IsInTrigger = false;
-
     public GameObject LockOnWall;
+
+    // Добавленные переменные для проверки взгляда
+    private Camera playerCamera;
+    private bool wasLookingAtDoor = false;
 
     void Start()
     {
         // Получаем компонент HingeJoint
         DoorRigidbody = GetComponentInChildren<Rigidbody>();
         DoorHingeJoint = GetComponentInChildren<HingeJoint>();
+
         if (DoorHingeJoint == null)
         {
             Debug.LogWarning("DoorHingeJoint не найден на объекте: " + gameObject.name);
@@ -29,49 +29,57 @@ public class DoorLockController : MonoBehaviour
         {
             Debug.LogWarning("DoorRigidBody не найден на объекте: " + gameObject.name);
         }
+
+        // Получаем камеру игрока
+        playerCamera = Camera.main;
     }
+
+    void Update()
+    {
+        if (IsInTrigger)
+        {
+            bool isLookingNow = IsPlayerLookingAtDoor();
+            
+            if (isLookingNow && !wasLookingAtDoor)
+            {
+                ShowDoorMessage();
+            }
+            else if (!isLookingNow && wasLookingAtDoor)
+            {
+                HideDoorMessages();
+            }
+            
+            wasLookingAtDoor = isLookingNow;
+        }
+    }
+
     void OnTriggerEnter(Collider PlayerCollider)
     {
-        // Проверяем, какой объект вошел в триггер
-        if (PlayerCollider.CompareTag("Player")) // Предположим, что у объекта есть тег "Player"
+        if (PlayerCollider.CompareTag("Player"))
         {
+            Debug.Log("finding bug 00");
+            Debug.Log(playerCamera.gameObject.name);
             CurrentDoor = gameObject.name;
             IsInTrigger = true;
-            if (LockOnWall != null)
-                MessageManager.Instance.ShowHint("Для взаимодействия с электронным замком нажмите E.");
-            else if (LockOnWall == null)
+            wasLookingAtDoor = IsPlayerLookingAtDoor();
+
+            if (wasLookingAtDoor)
             {
-                EnableDoor();
-                MessageManager.Instance.ShowMessage("Для двери не установлен электронный замок");
+                Debug.Log("finding bug 01");
+                ShowDoorMessage();
             }
         }
-
-    }
-
-    public void hideEnterPanel1()
-    {
-        MessageManager.Instance.ShowMessage("Доступ запрещен");
-        DisableDoor();
-        MessageManager.Instance.HideEnterPanel();
-    	
-    }
-    public void hideEnterPanel2() {
-    	MessageManager.Instance.ShowMessage("Доступ разрешен");
-        EnableDoor();
     }
 
     private void OnTriggerStay(Collider PlayerCollider)
     {
-        // Здесь можно выполнять действия, пока объект находится в триггере
-        //Debug.Log("Объект " + PlayerCollider.gameObject.name + " находится в триггере.");
-        if (PlayerCollider.CompareTag("Player")) // Предположим, что у объекта есть тег "Player"
+        if (PlayerCollider.CompareTag("Player"))
         {
-            if (LockOnWall != null && Input.GetKeyDown(KeyCode.E))
+            if (LockOnWall != null && Input.GetKeyDown(KeyCode.E) && IsPlayerLookingAtDoor())
             {
                 MessageManager.Instance.ShowEnterPanel();
-            
             }
-            else if (LockOnWall == null)
+            else if (LockOnWall == null && IsPlayerLookingAtDoor())
             {
                 EnableDoor();
             }
@@ -80,12 +88,58 @@ public class DoorLockController : MonoBehaviour
 
     private void OnTriggerExit(Collider PlayerCollider)
     {
-        if (PlayerCollider.CompareTag("Player")) // Предположим, что у объекта есть тег "Player"
+        if (PlayerCollider.CompareTag("Player"))
         {
             IsInTrigger = false;
-            MessageManager.Instance.HideHint();
+            wasLookingAtDoor = false;
+            HideDoorMessages();
             DisableDoor();
         }
+    }
+
+    private bool IsPlayerLookingAtDoor()
+    {
+        if (playerCamera == null) return false;
+
+        // Дополнительная проверка с Raycast для точности
+        RaycastHit hit;
+        if (Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out hit, 150f) && hit.collider.gameObject.name == "Interior_Door_Frame")
+        {
+            return true;
+        }
+        else return false;
+        
+    }
+
+    private void ShowDoorMessage()
+    {
+        if (LockOnWall != null)
+        {
+            MessageManager.Instance.ShowHint("Для взаимодействия с электронным замком нажмите E.");
+        }
+        else
+        {
+            EnableDoor();
+            MessageManager.Instance.ShowMessage("Для двери не установлен электронный замок");
+        }
+    }
+
+    private void HideDoorMessages()
+    {
+        MessageManager.Instance.HideHint();
+    }
+
+    public void hideEnterPanel1()
+    {
+        MessageManager.Instance.ShowMessage("Доступ запрещен");
+        DisableDoor();
+        MessageManager.Instance.HideEnterPanel();
+    }
+
+    public void hideEnterPanel2() 
+    {
+        MessageManager.Instance.ShowMessage("Доступ разрешен");
+        EnableDoor();
     }
 
     void EnableDoor()
@@ -100,7 +154,7 @@ public class DoorLockController : MonoBehaviour
 
     void DisableDoor()
     {
-        if (gameObject.name == CurrentDoor && DoorRigidbody != null && DoorRigidbody.isKinematic == false)
+        if (gameObject.name == CurrentDoor && DoorRigidbody != null && !DoorRigidbody.isKinematic)
         {
             DoorHingeJoint.useLimits = true;
             Debug.Log("useLimits включен у объекта: " + gameObject.name);
@@ -111,33 +165,28 @@ public class DoorLockController : MonoBehaviour
     {
         InteractiveObject obj = ObjectManager.Instance.GetObject(LockOnWall.name);
         Connection connection = ConnectionsManager.Instance.GetAllConnections(obj).FirstOrDefault();
+        
         if (connection != null)
         {
             InteractiveObject controller = connection.ObjectA == obj ? connection.ObjectB : connection.ObjectA;
             if (controller is AccessController accessController)
             {
-            
-            if (LockOnWall != null && status == 1)
-            {
-            
-                MessageManager.Instance.ShowMessage("Доступ запрещен");
+                if (LockOnWall != null && status == 1)
+                {
+                    MessageManager.Instance.ShowMessage("Доступ запрещен");
                     DisableDoor();
-            }
-            if (LockOnWall != null && status == 2)
+                }
+                if (LockOnWall != null && status == 2)
                 {
                     MessageManager.Instance.ShowMessage("Доступ разрешен");
                     EnableDoor();
                 }
-
             }
         }
-        else if (connection == null)
+        else
         {
             MessageManager.Instance.ShowMessage("Устройство не подключено к Контролеру");
             DisableDoor();
         }
     }
-
-
-
 }
